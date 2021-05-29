2b140149fa1c0451af495f98389d06df025c102c
Aleksandra Pivovarova <yourfreckle@mail.ru>
2021-05-29 23:51:54 +1000

    Задание 9. Сортировка стран и геозон

---
+++ csharp-example/csharp-example/AdminPanelTests/AdminPanelTests.cs
@@ -0,0 +1,153 @@
+﻿using NUnit.Framework;
+using OpenQA.Selenium;
+using System.Collections.Generic;
+
+namespace csharp_example
+{
+    [TestFixture]
+    [Parallelizable(ParallelScope.Fixtures)]
+    public class AdminPanelTests : AdminPanelTestBase
+    {
+        [OneTimeSetUp]
+        public void SetUp()
+        {
+            Login();
+        }
+
+        [Test]
+        public void AdminPanelMenuTest()
+        {
+            int i = 1;
+
+            while (true)
+            {
+                try
+                {
+                    IWebElement app = driver.FindElement(By.CssSelector($"li#app-:nth-child({i})"));
+                    app.Click();
+                    Assert.IsTrue(IsElementPresent(By.TagName("h1")));
+
+                    int j = 2;
+
+                    while (true)
+                    {
+                        try
+                        {
+                            IWebElement doc = driver.FindElement(By.CssSelector($"li[id^=doc-]:nth-child({j})"));
+                            doc.Click();
+                            Assert.IsTrue(IsElementPresent(By.TagName("h1")));
+                        }
+                        catch (NoSuchElementException)
+                        {
+                            break;
+                        }
+
+                        j++;
+                    }
+
+                }
+                catch (NoSuchElementException)
+                {
+                    break;
+                }
+
+                i++;
+            }
+        }
+
+        [Test]
+        public void AdminPanelCountriesTest()
+        {
+            driver.Url = "http://localhost/litecart/admin/?app=countries&doc=countries";
+
+            List<string> countries = new List<string>();
+            List<string> linksWithZone = new List<string>();
+
+            IList<IWebElement> countryRows = driver.FindElements(By.CssSelector("tr.row"));
+            foreach (IWebElement row in countryRows)
+            {
+                IList<IWebElement> cells = row.FindElements(By.TagName("td"));
+                countries.Add(cells[4].Text);
+
+                if (!cells[5].Text.Equals("0"))
+                {
+                    string href = cells[4].FindElement(By.TagName("a")).GetAttribute("href");
+                    linksWithZone.Add(href);
+                }
+            }
+
+            List<string> sortedContries = new List<string>(countries);
+            sortedContries.Sort();
+
+            Assert.AreEqual(countries, sortedContries);
+
+            foreach(string link in linksWithZone)
+            {
+                driver.Url = link;
+
+                List<string> zones = new List<string>();
+
+                IWebElement table = driver.FindElement(By.CssSelector("#table-zones.dataTable"));
+                IList<IWebElement>  zoneRows = table.FindElements(By.CssSelector("tr:not(.header)"));
+                foreach (IWebElement row in zoneRows)
+                {
+                    IList<IWebElement> cells = row.FindElements(By.TagName("td"));
+
+                    string name = cells[2].Text;
+
+                    if (name != string.Empty)
+                    {
+                        zones.Add(name);
+                    } 
+                }
+
+                List<string> sortedZones = new List<string>(zones);
+                sortedZones.Sort();
+
+                Assert.AreEqual(zones, sortedZones);
+            }
+        }
+
+        [Test]
+        public void AdminPanelGeoZonesTest()
+        {
+            driver.Url = "http://localhost/litecart/admin/?app=geo_zones&doc=geo_zones";
+
+
+            List<string> links = new List<string>();
+
+            IList<IWebElement> countryRows = driver.FindElements(By.CssSelector("tr.row"));
+            foreach (IWebElement row in countryRows)
+            {
+                IList<IWebElement> cells = row.FindElements(By.TagName("td"));
+                string href = cells[2].FindElement(By.TagName("a")).GetAttribute("href");
+                links.Add(href);
+            }
+
+            foreach (string link in links)
+            {
+                driver.Url = link;
+
+                List<string> zones = new List<string>();
+
+                IWebElement table = driver.FindElement(By.CssSelector("#table-zones.dataTable"));
+                IList<IWebElement>  zoneRows = table.FindElements(By.CssSelector("tr:not(.header)"));
+                foreach (IWebElement row in zoneRows)
+                {
+                    IList<IWebElement> cells = row.FindElements(By.TagName("td"));
+
+                    if (cells.Count > 1)
+                    {
+                        string name = cells[2].FindElement(By.CssSelector("option[selected=selected]")).Text;
+                        zones.Add(name);
+                    }
+                }
+
+                List<string> sortedZones = new List<string>(zones);
+                sortedZones.Sort();
+
+                Assert.AreEqual(zones, sortedZones);
+            }
+        }
+    }
+}
--- csharp-example/csharp-example/TestBase.cs
+++ csharp-example/csharp-example/TestBase.cs
@@ -1,5 +1,4 @@
 ﻿using System;
-using System.Threading;
 using NUnit.Framework;
 using OpenQA.Selenium;
 using OpenQA.Selenium.Chrome;
@@ -11,55 +10,50 @@ namespace csharp_example
 {
     public class TestBase
     {
-        public static ThreadLocal<IWebDriver> tlDriver = new ThreadLocal<IWebDriver>();
         public IWebDriver driver;
         public WebDriverWait wait;
 
-        [SetUp]
+        [OneTimeSetUp]
         public void Start()
         {
-            GetDriver();
+            driver = GetDriver();
         }
 
-        [TearDown]
+        [OneTimeTearDown]
         public void Stop()
         {
-            driver.Quit();
-            driver = null;
+            driver?.Quit();
         }
 
         public IWebDriver GetDriver()
         {
-            if (!tlDriver.IsValueCreated)
-            {
-                #region запуск Chrome
-                ChromeOptions options = new ChromeOptions();
-                options.PageLoadStrategy = PageLoadStrategy.Normal;
-                options.AddArgument("--start-maximized");
-                driver = new ChromeDriver(options);
-                #endregion
+            #region запуск Chrome
+            ChromeOptions options = new ChromeOptions();
+            options.PageLoadStrategy = PageLoadStrategy.Normal;
+            options.AddArgument("--start-maximized");
+            var driver = new ChromeDriver(options);
+            #endregion
 
-                #region запуск FF
-                //driver = new FirefoxDriver();
-                //driver.Manage().Window.Maximize();
-                #endregion
+            #region запуск FF
+            //driver = new FirefoxDriver();
+            //driver.Manage().Window.Maximize();
+            #endregion
 
-                #region запуск FF Nightly
-                //FirefoxDriverService service = FirefoxDriverService.CreateDefaultService();
-                //service.FirefoxBinaryPath = @"C:\Program Files\Firefox Nightly\firefox.exe";
-                //driver = new FirefoxDriver(service);
-                #endregion
+            #region запуск FF Nightly
+            //FirefoxDriverService service = FirefoxDriverService.CreateDefaultService();
+            //service.FirefoxBinaryPath = @"C:\Program Files\Firefox Nightly\firefox.exe";
+            //driver = new FirefoxDriver(service);
+            #endregion
 
-                #region запуск Edge
-                //driver = new EdgeDriver();
-                #endregion
+            #region запуск Edge
+            //driver = new EdgeDriver();
+            #endregion
 
-                tlDriver.Value = driver;
-                wait = new WebDriverWait(driver, TimeSpan.FromSeconds(10));
-                driver.Manage().Timeouts().ImplicitWait = TimeSpan.FromMilliseconds(500);
-                driver.Manage().Timeouts().PageLoad = TimeSpan.FromSeconds(10);
-            }
-            return tlDriver.Value;
+            wait = new WebDriverWait(driver, TimeSpan.FromSeconds(10));
+            driver.Manage().Timeouts().ImplicitWait = TimeSpan.FromMilliseconds(500);
+            driver.Manage().Timeouts().PageLoad = TimeSpan.FromSeconds(10);
+
+            return driver;
         }
 
         public bool IsElementPresent(By by)
