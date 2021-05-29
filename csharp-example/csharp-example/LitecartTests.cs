3ee94175d687ff41a852763189c675916160abfd
Aleksandra Pivovarova <yourfreckle@mail.ru>
2021-05-29 19:21:22 +1000

    Задание 8. Сценарий проверки стикеров у товаров

---
+++ csharp-example/csharp-example/LitecartTests.cs
@@ -0,0 +1,25 @@
+﻿using NUnit.Framework;
+using OpenQA.Selenium;
+using System.Collections.Generic;
+
+namespace csharp_example
+{
+    [TestFixture]
+    [Parallelizable(ParallelScope.Fixtures)]
+    public class LitecartTests : TestBase
+    {
+        [Test]
+        public void LitecartProductStickersTest()
+        {
+            driver.Url = "http://localhost/litecart/";
+
+            ICollection<IWebElement> products = driver.FindElements(By.ClassName("product"));
+
+            foreach (IWebElement product in products)
+            {
+                ICollection<IWebElement> stickers = product.FindElements(By.ClassName("sticker"));
+                Assert.AreEqual(1, stickers.Count);
+            }
+        }
+    }
+}
